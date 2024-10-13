f3bc729b85c74e422e435b63c6e8499f5d7f7784
Your Name <you@example.com>
2024-10-13 15:49:04 +0800

    newpost admin

---
+++ admin.js
@@ -0,0 +1,49 @@
+// @ts-check
+
+import express from "express";
+import dotenv from "dotenv";
+import fs from "fs";
+import http from "http";
+import https from "https";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { newpost } from "./admin/newpost.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const portssl = process.env.PORTSSL;
+
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "1GB",
+    parameterLimit: 1000000,
+  })
+);
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.route("/").get((req, res) => {
+  res.send("index page");
+});
+
+app.route("/").post(newpost);
+
+const httpServer = http.createServer(app);
+
+if (portssl) {
+  const privkeyPath = process.env.privkey || "./privkey.pem";
+  const certificatePath = process.env.certificate || "./fullchain.pem";
+  const privateKey = fs.readFileSync(privkeyPath, "utf8");
+  const certificate = fs.readFileSync(certificatePath, "utf8");
+  const credentials = { key: privateKey, cert: certificate };
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(portssl);
+}
+
+httpServer.listen(port);
+
+console.log("Express Started on: " + port);
---
+++ admin/newpost.js
@@ -0,0 +1,118 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+/**
+ *
+ * @param {string} text
+ */
+function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-"); // Replace multiple - with single -
+}
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const REPO_OWNER = "yokowasis";
+const REPO_NAME = "website-mtsn1kobi.sch.id";
+
+let TITLE = "Hello World";
+let excerpt = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.`;
+let coverImage = `/assets/blog/hello-world/cover.jpg`;
+let date = new Date().toISOString();
+let FILE_PATH = `_posts/${date}-${slugify(TITLE)}.md`;
+let author = {
+  name: "Tim Neutkens",
+  picture: "/assets/blog/authors/tim.jpeg",
+};
+let ogImage = {
+  url: "/assets/blog/hello-world/cover.jpg",
+};
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ */
+export const newpost = async (req, res) => {
+  const data = req.body;
+  // make sure data has all required fields
+  if (
+    !data?.title ||
+    !data?.excerpt ||
+    !data?.coverImage ||
+    !data?.date ||
+    !data?.author.name ||
+    !data?.author.picture ||
+    !data?.ogImage.url
+  ) {
+    res.status(400).json({ message: "Data tidak valid" });
+    return;
+  }
+
+  TITLE = data.title.replace(/'/g, "&#39;");
+  excerpt = data.excerpt.replace(/'/g, "&#39;");
+  coverImage = data.coverImage;
+  date = data.date;
+  FILE_PATH = `_posts/${date}-${slugify(TITLE)}.md`;
+  author.name = data.author.name.replace(/'/g, "&#39;");
+  author.picture = data.author.picture;
+  ogImage.url = data.ogImage.url;
+
+  const metadata = `---
+title: '${TITLE}'
+excerpt: '${excerpt}'
+coverImage: '${coverImage}'
+date: '${date}'
+author:
+  name: ${author.name}
+  picture: '${author.picture}'
+ogImage:
+  url: '${ogImage.url}'
+---
+
+`;
+
+  const postContent = data.postContent;
+
+  const content = metadata + postContent;
+  await createFile(content);
+  res.json({ message: "Post berhasil dibuat" });
+};
+
+/**
+ *
+ * @param {string} content
+ */
+async function createFile(content) {
+  const url = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`;
+
+  const data = {
+    message: "Add newpost.js",
+    content: Buffer.from(content).toString("base64"),
+    branch: "main", // Specify your branch
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+        Accept: "application/vnd.github.v3+json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    console.log("File created successfully:", responseData.content.html_url);
+  } catch (error) {
+    console.error("Error creating file:", error.message);
+  }
+}
