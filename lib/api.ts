2a2db52b6ad6478876a7ac4fcb774d83267f70db
Your Name <you@example.com>
2024-10-14 10:35:25 +0800

    halaman depan

--- lib/api.ts
+++ lib/api.ts
@@ -43,5 +43,7 @@ export function getAllPosts(fields: string[] = []) {
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+
+  // return top 8 posts
+  return posts.slice(0, 6);
 }
